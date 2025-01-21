fb4588025e51682b51b6e4ea654afa7686145351
Derrri <deribasec@gmail.com>
2025-01-21 12:34:05 +0300

    init

---
+++ src/__tests__/user.js
@@ -0,0 +1,36 @@
+import loadUser from "../loadUser";
+import { httpGet } from "../http";
+jest.mock('../http');
+
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
+test('should call loadUser once', () => {
+    httpGet.mockReturnValue(JSON.stringify({}));
+    loadUser(1);
+    expect(httpGet).toBeCalledWith('http://server:8080/users/1');
+});
+
+// Новый тест для демонстрации мокирования
+test('httpGet should be called and return mocked value', () => {
+    // Устанавливаем мок возвращаемого значения
+    httpGet.mockReturnValue(JSON.stringify({}));
+
+    // Вызовем мокированную функцию
+    const result = httpGet();
+
+    // Проверяем, что httpGet была вызвана
+    expect(httpGet).toBeCalled();
+    
+    // Проверяем, что возвращаемое значение соответствует ожидаемому
+    expect(result).toBe('{}');
+});
+
+// Новый тест для вызова оригинальной функции
+test('httpGet original function should return "{}"', () => {
+    const originalHttpGet = jest.requireActual('../http').httpGet;
+    const result = originalHttpGet(); // Вызов оригинальной функции
+
+    expect(result).toBe('{}'); // Проверяем, что возвращаемое значение соответствует ожидаемому
+});
